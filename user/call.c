960d80df6378b4cf8658e6d1cb7fe73326171df6
jensen-yan <yanyue171@mails.ucas.ac.cn>
2021-06-01 17:33:27 +0800

    finish proc 1, easy

--- user/call.c
+++ user/call.c
@@ -12,6 +12,9 @@ int f(int x) {
 }
 
 void main(void) {
-  printf("%d %d\n", f(8)+1, 13);
+  // printf("%d %d\n", f(8)+1, 13);
+  unsigned int i = 0x00646c72;
+  printf("H%x Wo%s", 57616, &i);
+  printf("x=%d, y = %d", 3);
   exit(0);
 }
